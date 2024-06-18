7c65c8cc683f8a8621047524df822af91efb522a
Daniel Milenov <danimileniv@gmail.com>
2024-06-18 17:40:11 +0200

    Add breakpoints xs and xxs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,json}'],
 	theme: {
 		extend: {},
+		screens: {
+			'xs': '500px',
+			'xxs': '450px',
+			...defaultTheme.screens,
+		  },
 	},
 	plugins: [],
 }
