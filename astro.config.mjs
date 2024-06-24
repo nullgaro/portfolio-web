99341fc511e6e1603c9b2f2963984a865a5ff2f7
Daniel Milenov <danimileniv@gmail.com>
2024-06-24 13:29:58 +0200

    Add sitemap for better seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://nullgaro.com",
+  integrations: [tailwind(), sitemap()],
   i18n: {
     defaultLocale: "es",
-    locales: ["es", "en"],
+    locales: ["es", "en"]
   }
-  // Robots.txt
 });
