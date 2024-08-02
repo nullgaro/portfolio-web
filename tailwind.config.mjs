9a87bb3fe4fdfa7cb9964128dc75bd896f9f8cfc
Daniel Milenov <danimileniv@gmail.com>
2024-08-02 20:27:49 +0200

    Fix background

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,22 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,json}'],
 	darkMode: "class",
 	theme: {
-		extend: {},
+		extend: {
+			dropShadow: {
+				'around-light': [
+					'2px 0 1px rgba(255, 255, 255, 0.5)',
+					'-2px 0 1px rgba(255, 255, 255, 0.5)',
+					'0 2px 1px rgba(255, 255, 255, 0.5)',
+					'0 -2px 1px rgba(255, 255, 255, 0.5)'
+				],
+				'around-dark': [
+					'2px 0 1px rgba(0, 0, 0, 1)',
+					'-2px 0 1px rgba(0, 0, 0, 1)',
+					'0 2px 1px rgba(0, 0, 0, 1)',
+					'0 -2px 1px rgba(0, 0, 0, 1)'
+				]
+			  }
+		},
 		screens: {
 			'xs': '500px',
 			'xxs': '450px',
